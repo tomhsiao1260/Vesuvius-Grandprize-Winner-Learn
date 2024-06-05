44119511788b841a640595a018e1b736b8ea5f56
Hsiao Yao <tomtogo1260@gmail.com>
2024-06-05 19:15:51 +0800

    model init

--- inference_timesformer.py
+++ inference_timesformer.py
@@ -2,6 +2,10 @@ import torch
 import pytorch_lightning as pl
 import numpy as np
 
+class RegressionPLModel(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        print("model init")
 
 if __name__ == "__main__":
-    pass
+    model=RegressionPLModel()
