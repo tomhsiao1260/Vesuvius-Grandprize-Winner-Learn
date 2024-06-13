d2a0c32036c115140d54ea086a4cd0303616e7dd
Hsiao Yao <tomtogo1260@gmail.com>
2024-06-13 19:19:42 +0800

    remove unused import, use CFG config, space formatting, garbage collection

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import gc
 import torch
 import numpy as np
 import scipy.stats as st
@@ -11,34 +12,40 @@ from torch.utils.data import Dataset, DataLoader
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-segment_path = 'train_scrolls'
-# fragment_id = '20230509182749'
-fragment_id = 'pi_small'
-checkpoint_path = 'checkpoints/timesformer_wild15_20230702185753_0_fr_i3depoch=12.ckpt'
-
-in_chans = 26
-tile_size = 64
-stride = tile_size // 3
-batch_size = 8
-# batch_size = 256
-num_workers = 4
-size = 64
-
-device_type = 'cpu'
+device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
 device = torch.device(device_type)
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+segment_path = 'train_scrolls'
+fragment_id = 'pi_small'
+# fragment_id = '20230509182749'
+model_path = 'checkpoints/timesformer_wild15_20230702185753_0_fr_i3depoch=12.ckpt'
 
 def gkern(kernlen=21, nsig=3):
-    """Returns a 2D Gaussian kernel."""
-    x = np.linspace(-nsig, nsig, kernlen+1)
-    kern1d = np.diff(st.norm.cdf(x))
-    kern2d = np.outer(kern1d, kern1d)
-    return kern2d / kern2d.sum()
+  """Returns a 2D Gaussian kernel."""
+  x = np.linspace(-nsig, nsig, kernlen+1)
+  kern1d = np.diff(st.norm.cdf(x))
+  kern2d = np.outer(kern1d, kern1d)
+  return kern2d / kern2d.sum()
+
+class CFG:
+  in_chans = 26 # 65
+
+  size = 64
+  tile_size = 64
+  stride = tile_size // 3
+
+  batch_size = 8
+  # batch_size = 256
+
+  num_workers = 4
+  # num_workers = 16
 
 def read_image_mask(fragment_id, start_idx=18, end_idx=38, rotation=0):
   image_stack = []
   mid = 65 // 2
-  start = mid - in_chans // 2
-  end = mid + in_chans // 2
+  start = mid - CFG.in_chans // 2
+  end = mid + CFG.in_chans // 2
 
   for i in range(start_idx, end_idx):
     image = cv2.imread(f"{segment_path}/{fragment_id}/layers/{i:02}.tif", 0)
@@ -47,7 +54,7 @@ def read_image_mask(fragment_id, start_idx=18, end_idx=38, rotation=0):
     image = np.pad(image, [(0, pad0), (0, pad1)], constant_values=0)
     image = np.clip(image, 0, 200)
     image_stack.append(image)
-
+  
   image_stack = np.stack(image_stack, axis=2)
   fragment_mask = cv2.imread(f"{segment_path}/{fragment_id}/{fragment_id}_mask.png", 0)
   fragment_mask = np.pad(fragment_mask, [(0, pad0), (0, pad1)], constant_values=0)
@@ -59,49 +66,50 @@ def get_img_splits(fragment_id, start_idx, end_idx, rotation=0):
 
   images = []
   coords = []
-  x_list = list(range(0, image_stack.shape[1]-tile_size+1, stride))
-  y_list = list(range(0, image_stack.shape[0]-tile_size+1, stride))
+  x_list = list(range(0, image_stack.shape[1]-CFG.tile_size+1, CFG.stride))
+  y_list = list(range(0, image_stack.shape[0]-CFG.tile_size+1, CFG.stride))
 
   for ymin in y_list:
     for xmin in x_list:
-      ymax = ymin + tile_size
-      xmax = xmin + tile_size
+      ymax = ymin + CFG.tile_size
+      xmax = xmin + CFG.tile_size
       if not np.any(fragment_mask[ymin:ymax, xmin:xmax]==0):
         images.append(image_stack[ymin:ymax, xmin:xmax])
         coords.append([xmin, ymin, xmax, ymax])
 
   transform = A.Compose([
-    A.Resize(size, size),
-    A.Normalize(mean= [0] * in_chans, std= [1] * in_chans),
+    A.Resize(CFG.size, CFG.size),
+    A.Normalize(mean= [0] * CFG.in_chans, std= [1] * CFG.in_chans),
     ToTensorV2(transpose_mask=True),
   ])
 
   coords = np.stack(coords)
-  dataset = CustomDatasetTest(images, coords, transform=transform)
-  # dataset = CustomDatasetTest(images[:1000], coords[:1000], transform=transform)
-  loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True, drop_last=False)
+  dataset = CustomDatasetTest(images, coords, CFG, transform=transform)
+  # dataset = CustomDatasetTest(images[:1000], coords[:1000], CFG, transform=transform)
+  loader = DataLoader(dataset, batch_size=CFG.batch_size, shuffle=False, num_workers=CFG.num_workers, pin_memory=True, drop_last=False)
   image_shape = (image_stack.shape[0], image_stack.shape[1])
 
   return loader, coords, image_shape, fragment_mask
 
 class CustomDatasetTest(Dataset):
-    def __init__(self, images, coords, transform=None):
-        self.images = images
-        self.coords = coords
-        self.transform = transform
+  def __init__(self, images, coords, cfg, transform=None):
+    self.images = images
+    self.coords = coords
+    self.cfg = cfg
+    self.transform = transform
 
-    def __len__(self):
-        return len(self.images)
-    
-    def __getitem__(self, idx):
-        image = self.images[idx]
-        coord = self.coords[idx]
+  def __len__(self):
+    return len(self.images)
 
-        if self.transform:
-          data = self.transform(image=image)
-          image = data['image'].unsqueeze(0) 
+  def __getitem__(self, idx):
+    image = self.images[idx]   
+    coord = self.coords[idx]
 
-        return image, coord
+    if self.transform:
+      data = self.transform(image=image)    
+      image = data['image'].unsqueeze(0)   
+
+    return image, coord
 
 class RegressionPLModel(pl.LightningModule):
   def __init__(self, pred_shape, size=64, enc='', with_norm=False):
@@ -128,7 +136,7 @@ class RegressionPLModel(pl.LightningModule):
     return x
 
 def predict_fn(loader, model, device, image_shape):
-  kernel = gkern(size, 1)
+  kernel = gkern(CFG.size, 1)
   kernel = kernel / kernel.max()
 
   mask_pred = np.zeros(image_shape)
@@ -148,7 +156,7 @@ def predict_fn(loader, model, device, image_shape):
     for i, (x1, y1, x2, y2) in enumerate(coords):
       # mask_pred[y1:y2, x1:x2] += images[i, :, 12, :, :].squeeze().numpy()
       mask_pred[y1:y2, x1:x2] += np.multiply(F.interpolate(y_preds[i].unsqueeze(0).float(), scale_factor=16, mode='bilinear').squeeze(0).squeeze(0).numpy(), kernel)
-      mask_count[y1:y2, x1:x2] += np.ones((size, size))
+      mask_count[y1:y2, x1:x2] += np.ones((CFG.size, CFG.size))
 
     filename = f"./predict/{fragment_id}_{step}.png"
     image_save(filename, mask_pred / mask_count)
@@ -163,14 +171,16 @@ def image_save(filename, data):
   cv2.imwrite(filename, data)
 
 if __name__ == "__main__":
-  model = RegressionPLModel.load_from_checkpoint(checkpoint_path, map_location=device, strict=False)
+  model = RegressionPLModel.load_from_checkpoint(model_path, map_location=device, strict=False)
+  if (device_type == 'cuda'): model.cuda()
   model.eval()
 
-  if (device_type == 'cuda'): model.cuda()
-
   start_idx = 17
-  end_idx = start_idx + in_chans
-
+  end_idx = start_idx + 26
   loader, coords, image_shape, fragment_mask = get_img_splits(fragment_id, start_idx, end_idx)
 
   predict_fn(loader, model, device, image_shape)
+
+  del loader, model
+  torch.cuda.empty_cache()
+  gc.collect()
